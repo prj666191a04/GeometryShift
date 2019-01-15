ec68cf5c8a931d6d3342b4ac1be5a18f4d4abbf4
azou1 <itchylol742@gmail.com>
2019-01-15 14:41:22 -0500

    basic movement added

---
+++ GeomotryShiftV1/Assets/_Scripts/MyFirstScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string printText = "default text";
+    public int counter = 0;
+    void Start()
+    {
+        print(printText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter % 10 == 0)
+        {
+            print(counter);
+        }
+    }
+}
---
+++ GeomotryShiftV1/Assets/_Scripts/RigidBodyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyController : MonoBehaviour
+{
+    public Rigidbody theRB;
+    public float speedMultiplier = 6;
+    public float jumpForce = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 myVector = new Vector3(0, 0, 0);
+        myVector.x = Input.GetAxis("Horizontal");
+        myVector.y = theRB.velocity.y;
+        myVector.z = Input.GetAxis("Vertical");
+        myVector.x *= speedMultiplier;
+        myVector.z *= speedMultiplier;
+        theRB.velocity = myVector;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        }
+    }
+}
