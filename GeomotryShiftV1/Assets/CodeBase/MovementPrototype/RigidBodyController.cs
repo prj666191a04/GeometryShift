2c21bb4b626690f366c103b56c85f5e710b6697d
azou1 <azou1@myseneca.com>
2019-01-16 16:50:57 -0500

    added ground detection, double jump, some documentation
    added ground detection, double jump, some documentation

--- GeomotryShiftV1/Assets/CodeBase/MovementPrototype/RigidBodyController.cs
+++ GeomotryShiftV1/Assets/CodeBase/MovementPrototype/RigidBodyController.cs
@@ -5,28 +5,133 @@ using UnityEngine;
 public class RigidBodyController : MonoBehaviour
 {
     public Rigidbody theRB;
-    public float speedMultiplier = 6;
-    public float jumpForce = 8f;
+    public float speedMultiplier_ = 6;
+    public float jumpForce_ = 5.5f;
+    public int numberOfMidairJumpsAllowed_ = 1;//0 = no midair jump, 1 = double jump, 2 = triple jump, etc
+    public int numberOfMidairJumpsRemaining_ = 1;
+    float sqrtOfZeroPointFive_ = 0.7071067811865475f;
+
     // Start is called before the first frame update
     void Start()
     {
         theRB = GetComponent<Rigidbody>();
     }
 
+
+    bool isGrounded()
+    {
+        float compensationRange = 0.7f;
+        float distanceToGroundToQualityAsGrounded = 1.1f;
+        Vector3 positionVector = new Vector3(0, 0, 0);
+        Vector3 originalPositionVector = new Vector3(0, 0, 0);
+        positionVector = transform.position;
+        originalPositionVector = transform.position;
+        if (Physics.Raycast(positionVector, -Vector3.up, distanceToGroundToQualityAsGrounded))
+        {
+            return true;
+        }
+
+
+        //compensation ground detection below
+        //explained in compensationRangeExplanation.png
+
+        positionVector.x += compensationRange;
+        if (Physics.Raycast(positionVector, -Vector3.up, distanceToGroundToQualityAsGrounded))
+        {
+            return true;
+        }
+
+        positionVector = originalPositionVector;
+        positionVector.x -= compensationRange;
+        if (Physics.Raycast(positionVector, -Vector3.up, distanceToGroundToQualityAsGrounded))
+        {
+            return true;
+        }
+
+        positionVector = originalPositionVector;
+        positionVector.z += compensationRange;
+        if (Physics.Raycast(positionVector, -Vector3.up, distanceToGroundToQualityAsGrounded))
+        {
+            return true;
+        }
+
+        positionVector = originalPositionVector;
+        positionVector.z -= compensationRange;
+        if (Physics.Raycast(positionVector, -Vector3.up, distanceToGroundToQualityAsGrounded))
+        {
+            return true;
+        }
+
+        //now the diagonals
+
+        positionVector = originalPositionVector;
+        positionVector.x += compensationRange * sqrtOfZeroPointFive_;
+        positionVector.z += compensationRange * sqrtOfZeroPointFive_;
+        if (Physics.Raycast(positionVector, -Vector3.up, distanceToGroundToQualityAsGrounded))
+        {
+            return true;
+        }
+
+        positionVector = originalPositionVector;
+        positionVector.x += compensationRange * 0.7f;
+        positionVector.z -= compensationRange * sqrtOfZeroPointFive_;
+        if (Physics.Raycast(positionVector, -Vector3.up, distanceToGroundToQualityAsGrounded))
+        {
+            return true;
+        }
+
+        positionVector = originalPositionVector;
+        positionVector.x -= compensationRange * 0.7f;
+        positionVector.z += compensationRange * sqrtOfZeroPointFive_;
+        if (Physics.Raycast(positionVector, -Vector3.up, distanceToGroundToQualityAsGrounded))
+        {
+            return true;
+        }
+
+        positionVector = originalPositionVector;
+        positionVector.x -= compensationRange * 0.7f;
+        positionVector.z -= compensationRange * sqrtOfZeroPointFive_;
+        if (Physics.Raycast(positionVector, -Vector3.up, distanceToGroundToQualityAsGrounded))
+        {
+            return true;
+        }
+
+
+        return false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 myVector = new Vector3(0, 0, 0);
-        myVector.x = Input.GetAxis("Horizontal");
-        myVector.y = theRB.velocity.y;
-        myVector.z = Input.GetAxis("Vertical");
-        myVector.x *= speedMultiplier;
-        myVector.z *= speedMultiplier;
-        theRB.velocity = myVector;
+        Vector3 movementVector = new Vector3(0, 0, 0);
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector.y = theRB.velocity.y;
+        movementVector.z = Input.GetAxis("Vertical");
 
-        if (Input.GetButtonDown("Jump"))
+        movementVector.x *= speedMultiplier_;
+        movementVector.z *= speedMultiplier_;
+
+        theRB.velocity = movementVector;
+
+        bool isGroundedThisFrame = isGrounded();//so it only runs once per frame
+
+        if (isGroundedThisFrame)//reset the number of air jumps
         {
-            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+            numberOfMidairJumpsRemaining_ = numberOfMidairJumpsAllowed_;
         }
+
+        if (Input.GetButtonDown("Jump"))//the player attempts to jump
+        {
+            if (isGroundedThisFrame)//the player can jump because they are on the ground
+            {
+                theRB.velocity = new Vector3(theRB.velocity.x, jumpForce_, theRB.velocity.z);
+            }
+            else if (numberOfMidairJumpsRemaining_ > 0)//the player can air-jump
+            {
+                theRB.velocity = new Vector3(theRB.velocity.x, jumpForce_, theRB.velocity.z);
+                numberOfMidairJumpsRemaining_--;
+            }
+        }
+
     }
 }
