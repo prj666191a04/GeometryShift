7bf99dbdebeff3f70d070a851adcb62e4fd06ddf
azou1 <azou1@myseneca.com>
2019-01-30 14:37:30 -0500

    basic setup for obstacle testing
    basic setup for obstacle testing

---
+++ GeomotryShiftV1/Assets/CodeBase/EnemiesAndObstacles/DisspearingPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisspearingPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            print("touched player ");
+            other.gameObject.GetComponent<CStatus>().Damage(1.2f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            print("stopped touching player ");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GeomotryShiftV1/Assets/LoadHubWithoutPressingButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadHubWithoutPressingButton : MonoBehaviour
+{
+    public GameObject theEnvironmentContainer;
+    public GameObject theLevelToBeLoadedImmediately;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelLoader a = new LevelLoader();
+        theLevelToBeLoadedImmediately.transform.parent = theEnvironmentContainer.transform;
+        a.EnvironmentContainer = theEnvironmentContainer.transform;
+        a.LoadLevel(theLevelToBeLoadedImmediately);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
