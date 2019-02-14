0b163b589f11ce582247b9b48887f37a8a50fdda
atilla17 <42915217+atilla17@users.noreply.github.com>
2019-02-14 00:53:24 -0500

    Pause menue started
    Basic pause menue to be used anywhere in the game except for the main menue.
    
    Features still not compleete
    functioning sub menue's
    alturnate keyboard controls
    UI sounds
    Link to escape key
    Pause game when opened.

---
+++ GeomotryShiftV1/Assets/CodeBase/UI/PMTab.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMTab : MonoBehaviour
+{
+    public int position;
+
+    public GameObject subMenue;
+    public Transform subContainer;
+
+    public static PMTab selected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(position == 0)
+        {
+            selected = this;
+        }
+    }
+
+
+    public virtual void Select()
+    {
+        if (PMTopLevel.loadedSubMenue != null)
+             Destroy(PMTopLevel.loadedSubMenue);
+
+        if (subMenue != null)
+            PMTopLevel.loadedSubMenue = Instantiate(subMenue, subContainer);
+        else
+            Debug.LogWarning("subContainer not set");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
