c79a041265a6c3b2230f7e208a46f23ed7c39aff
boohar02 <46663574+boohar02@users.noreply.github.com>
2019-02-18 17:49:08 -0500

    Fixed and added new inventory ui

---
+++ GeomotryShiftV1/Assets/CodeBase/NewInventory/Inventory2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item2
+{
+    public string itemName;
+    public Sprite icon;
+    public float price = 1f;
+
+}
+
+
+public class Inventory2 : MonoBehaviour
+{
+
+    public List<Item2> itemList;
+    public Transform contentPanel;
+    public Inventory2 otherInv;
+    public Text myGoldDisplay;
+    public SimpleObjectPool buttonObjectPool;
+    public float gold = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void AddButtons()
+    {
+
+    }
+}
