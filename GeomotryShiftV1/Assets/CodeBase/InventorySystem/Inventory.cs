e229e32a847696f0ab92c5133be1926196d2d08d
atilla17 <42915217+atilla17@users.noreply.github.com>
2019-02-19 15:18:49 -0500

    pushed harleys inventory to master

---
+++ GeomotryShiftV1/Assets/CodeBase/InventorySystem/Inventory.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Smart inventroy allowed to add and remove items
+
+
+[System.Serializable]
+public class TestItem
+{
+    public string itemName;
+    public Sprite icon;
+    public float value = 1f;
+}
+
+
+
+public class Inventory : MonoBehaviour
+{
+    //Main inventory variable
+    public List<TestItem> itemlist;
+    public SimpleObjectPool buttonpool;
+    public Transform itemContenetPanel;
+    public GameObject prefab;
+
+
+    //Desc stuff
+    public SimpleObjectPool descriptionPool;
+    public Transform descContenetPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        refreshDispaly();
+    }
+
+    public void refreshDispaly()
+    {
+        //goldDisplay.text = "Gold: " + gold.ToString();
+        removeButton();
+        addButton();
+
+    }
+
+    private  void addButton()
+    {
+        for (int i = 0; i < itemlist.Count; i++)
+        {
+            
+            TestItem item = itemlist[i];
+            //GameObject newbutton = buttonpool.GetObject();//grabs an opbject from the pool to use  
+
+            Instantiate(prefab, itemContenetPanel);
+
+            //newbutton.transform.SetParent(itemContenetPanel); // assigns the object to the inventory panel 
+            
+            
+            
+            ItemSlot itemSlot = prefab.GetComponent<ItemSlot>(); // gets the new game instance 
+            itemSlot.setup(item, this); // sents to inner function to assign values
+        }
+    }
+
+    private void removeButton()
+    {
+        while (itemContenetPanel.childCount > 0)
+        {
+            GameObject toRemove = itemContenetPanel.transform.GetChild(0).gameObject; //Aslong as there is an object in the invetory there will always b a child (0)
+            buttonpool.ReturnObject(toRemove); // Returns the object back to the object pool for later use
+        }
+    }
+
+    
+    public void loadDescription(TestItem item)
+    {
+
+        
+        if (descContenetPanel.childCount > 0)
+        {
+            GameObject toRemove = descContenetPanel.transform.GetChild(0).gameObject; //Aslong as there is an object in the invetory there will always b a child (0)
+            descriptionPool.ReturnObject(toRemove); // Returns the object back to the object pool for later use
+        }
+
+        GameObject newDescPoolObj = descriptionPool.GetObject();
+        newDescPoolObj.transform.SetParent(descContenetPanel);
+
+        descriptionBox newDesc = newDescPoolObj.GetComponent<descriptionBox>();
+        newDesc.setup(item);
+    }
+
+
+    private void addItem(TestItem itemToAdd, Inventory inventory)
+    {
+        inventory.itemlist.Add(itemToAdd);
+    }
+    private void removeItem(TestItem itemToRemove, Inventory inventory)
+    {
+        for (int i = inventory.itemlist.Count - 1; i >= 0; i--)//Allows for safe item removal. Will not mess up the count and cause an overflow/out of index crash
+        {
+            if (inventory.itemlist[i] == itemToRemove)
+                inventory.itemlist.RemoveAt(i);
+        }
+    }
+
+}
---
+++ GeomotryShiftV1/Assets/CodeBase/InventorySystem/ItemSlot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Semi smart item slot
+//Holds info of item for smarter inventory
+//Decription is accessed through the item slot its self
+
+public class ItemSlot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button button;
+    public Text itemName;
+    public Text itemDesc;
+    public Image icon;
+    public GameObject descriptionPannel;
+
+    private TestItem item;
+    private Inventory inventory;
+    private descriptionBox description;
+    
+
+    //public GameObject descriptionPanel; // UI Panel - used to show and hide the UI
+
+    void Start()
+    {
+        button.onClick.AddListener(handleClick);
+    }
+
+    public void setup(TestItem currentIten, Inventory currentInventory)
+    {
+        item = currentIten;
+        itemName.text = item.itemName;
+        itemDesc.text = item.value.ToString();
+        icon.sprite = item.icon;
+
+        inventory = currentInventory;
+        Debug.Log("In Setup");
+        Debug.Log(item.itemName);
+        Debug.Log(item.value.ToString());
+
+    }
+
+    public void handleClick()
+    {
+        inventory.loadDescription(item);
+    }
+
+
+}
