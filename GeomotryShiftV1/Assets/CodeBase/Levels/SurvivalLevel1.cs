02e9b85882234e4df8557f5a5991d98892c006ba
azou1 <azou1@myseneca.com>
2019-02-24 14:36:09 -0500

    add survival level with timer (no enemies yet)

---
+++ GeomotryShiftV1/Assets/CodeBase/Levels/SurvivalLevel1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurvivalLevel1 : MonoBehaviour
+{
+    float secondsPassed = 0;
+    public float secondsNeededToSurviveToWin = 3;
+    Random random = new Random();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void whatEnemiesShouldSpawn()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        secondsPassed += Time.deltaTime;
+        print(secondsPassed + "rng 1-3 " + Random.Range(1,3));
+        if (secondsPassed > secondsNeededToSurviveToWin)
+        {
+            LevelBase.instance.AcknowledgeLevelCompletion();
+        }
+    }
+}
