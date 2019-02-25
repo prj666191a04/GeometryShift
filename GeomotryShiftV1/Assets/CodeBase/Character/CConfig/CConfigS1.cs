d2cfaaa3050bab113270a990a8f750083ccd0c0b
atilla17 <42915217+atilla17@users.noreply.github.com>
2019-02-24 22:46:43 -0500

    started new level

---
+++ GeomotryShiftV1/Assets/CodeBase/Character/CConfig/CConfigS1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CConfigS1 : CConfig
+{
+    public GameObject mapFlow;
+
+    public override void SetupCharacter(GameObject playerPrefab, Transform spawnPoint, GameObject parentObject)
+    {
+        GameObject player = GameObject.Instantiate(playerPrefab, spawnPoint.position, Quaternion.identity, parentObject.transform);
+        CController cc = player.GetComponent<CController>();
+        //in future will call a configure script for next step this is a hard coded and temporary solution
+
+        if (player.GetComponent<Rigidbody>())
+        {
+            cc.rBody = player.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            cc.rBody = player.AddComponent<Rigidbody>();
+        }
+
+
+        cc.motorPool = new CMotor[1];
+        cc.motorPool[0] = player.AddComponent<FloatMotorA>();
+        cc.AssignMotor(cc.motorPool[0]);
+        cc.motor.SetPhysics(cc.rBody);
+
+        player.AddComponent<CStatusA>();
+        Debug.Log("Character Ready");
+        //temporary
+        //PlayerReady(player);
+        GeometryShift.instance.cameraController.SetTarget(mapFlow);
+        GeometryShift.instance.cameraController.Init(new Vector3(0f, 45f, 0));
+        GeometryShift.instance.cameraController.LookAt(player);
+
+
+    }
+}
---
+++ GeomotryShiftV1/Assets/CodeBase/Character/Motors/FloatMotorA.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatMotorA : CMotor
+{
+
+    Quaternion newRotation;
+    Quaternion targetRotation;
+    Vector3 targetDirection;
+
+    private void Start()
+    {
+        newRotation = transform.rotation;
+        targetRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        RotateToDirection();
+    }
+
+    void FixedUpdate()
+    {
+        rBody.MovePosition(transform.position + new Vector3(h_, 0, v_) * 0.3f);
+        
+    }
+
+    void RotateToDirection()
+    {
+
+        //Get direction of input
+        targetDirection = new Vector3(h_, 0, v_);
+
+        if (targetDirection != Vector3.zero)
+        {
+            //Get input direction relative to camera logic not working
+            //Vector3 relativeDirection = GeometryShift.instance.cameraController.gameObject.transform.TransformDirection(targetDirection);
+            //Remove Cameras Y from the Direction
+            //relativeDirection.Set(relativeDirection.x, 0, relativeDirection.z);
+            //Set the target Direction
+            //targetRotation = Quaternion.LookRotation(relativeDirection, Vector3.up);
+
+            //temporary until above code is fixed
+            targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+
+            //Preform this frames rotation
+            
+        }
+
+        //Apply the value
+        newRotation = Quaternion.Lerp(newRotation, targetRotation, Time.deltaTime * 10);
+        transform.rotation = newRotation;
+    }
+
+
+    protected override void ConfigurePhysics()
+    {
+        rBody.useGravity = false;
+        rBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionY;
+        rBody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+        rBody.interpolation = RigidbodyInterpolation.Interpolate;
+        rBody.mass = 1;
+    }
+
+    //posible that this is not needed
+    //void OnEnable()
+    //{
+    //    //On first enable this object will be null but 
+    //    //we need to be able to run the following code
+    //    //if it is to be enabled multiple times
+    //    if (rBody != null)
+    //    {
+    //        ConfigurePhysics();
+    //    }
+
+    //}
+}
