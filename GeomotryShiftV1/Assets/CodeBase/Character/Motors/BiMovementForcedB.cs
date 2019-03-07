a41e8cc04ad2b14b8453aa9fc72d933fa3ff8050
boohar02 <46663574+boohar02@users.noreply.github.com>
2019-03-07 17:49:18 -0500

    LevelD start of new movement

---
+++ GeomotryShiftV1/Assets/CodeBase/Character/Motors/BiMovementForcedB.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiMovementForcedB : CMotor
+{
+    public Rigidbody theRB;
+    public float speedMultiplier_ = 6f;
+    public float jumpStrength_ = 30f;
+
+    void FixedUpdate()
+    {
+        Vector3 movementVector = new Vector3(speedMultiplier_, 0, 0);
+
+        movementVector.y = Input.GetAxis("Vertical") * jumpStrength_;
+
+        rBody.AddForce(movementVector);
+
+    }
+
+
+    protected override void ConfigurePhysics()
+    {
+        //rBody = GetComponent<Rigidbody>();
+        rBody.constraints =
+            RigidbodyConstraints.FreezePositionZ |
+            RigidbodyConstraints.FreezeRotationZ |
+            RigidbodyConstraints.FreezeRotationX |
+            RigidbodyConstraints.FreezeRotationY |
+            RigidbodyConstraints.FreezePositionZ;
+
+        //rBody.rotation.Equals(Quaternion.Euler(new Vector3(0, 0, 0)));
+
+        rBody.useGravity = false;
+        rBody.interpolation = RigidbodyInterpolation.Interpolate;
+        
+    }
+}
