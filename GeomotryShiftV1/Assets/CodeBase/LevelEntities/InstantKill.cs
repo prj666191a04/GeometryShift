da243aa053a2c4fb5ae86690f312280193ef56c1
boohar02 <46663574+boohar02@users.noreply.github.com>
2019-03-10 13:22:45 -0400

    Started working on Level E

--- GeomotryShiftV1/Assets/CodeBase/LevelEntities/InstantKill.cs
+++ GeomotryShiftV1/Assets/CodeBase/LevelEntities/InstantKill.cs
@@ -6,8 +6,11 @@ public class InstantKill : MonoBehaviour
 {
     private void OnTriggerEnter(Collider collision)
     {
+        Debug.Log(collision.gameObject.tag);
         if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("Kill");
+            collision.gameObject.GetComponent<CStatus>().Damage(9999f);
             collision.gameObject.GetComponent<CStatus>().Damage(9999f);
         }
     }
