62017ffd0328002220f6de1b6cad4ca602b79713
atilla17 <42915217+atilla17@users.noreply.github.com>
2019-03-10 10:54:12 -0400

    Inported new model and also inported one of allens scripts that was missing

---
+++ GeomotryShiftV1/Assets/CodeBase/Levels/SurvivalLevel1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurvivalLevel1 : MonoBehaviour
+{
+    float secondsPassed = 0;
+    float secondsNeededToSurviveToWin = 80;
+    Random random = new Random();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*secondsPassed += Time.deltaTime;
+        if (secondsPassed > secondsNeededToSurviveToWin)
+        {
+            LevelBase.instance.AcknowledgeLevelCompletion();
+        }*/
+    }
+}
