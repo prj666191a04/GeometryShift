59fe19fca9dae03bbf965a87dbb2af57cee0142d
boohar02 <46663574+boohar02@users.noreply.github.com>
2019-03-10 12:07:44 -0400

    Created and added Instant kill to level D

---
+++ GeomotryShiftV1/Assets/CodeBase/LevelEntities/InstantKill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantKill : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<CStatus>().Damage(9999f);
+        }
+    }
+}
