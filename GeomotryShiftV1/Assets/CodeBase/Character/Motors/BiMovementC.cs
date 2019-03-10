aca7d9e7801a2b4282b0575a4d11495a6278af58
boohar02 <46663574+boohar02@users.noreply.github.com>
2019-03-10 16:46:18 -0400

    Created movement and controler script for lvlE

--- GeomotryShiftV1/Assets/CodeBase/Character/Motors/BiMovementC.cs
+++ GeomotryShiftV1/Assets/CodeBase/Character/Motors/BiMovementC.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BiMovementForcedB : CMotor
+public class BiMovementC : CMotor
 {
     public Rigidbody theRB;
     public float jumpStrength_ = 45f;
     public float maxSpeed_ = 7.5f;
     public float speed_ = 0;
-    bool availableJump = true;
     public float rayDistance = 1f;
 
 
@@ -19,33 +18,12 @@ public class BiMovementForcedB : CMotor
         {
             speed_ = maxSpeed_;
         }
-        Vector3 movementVector = new Vector3(maxSpeed_-speed_, 0, 0);
+        Vector3 movementVector = new Vector3(maxSpeed_ - speed_, 0, 0);
 
-        if (Input.GetKey(KeyCode.Space) && availableJump)
+        if (Input.GetKey(KeyCode.Space))
         {
             rBody.AddForce(rBody.transform.TransformDirection(Vector3.left) * jumpStrength_);
-            availableJump = false;
         }
-
-        RaycastHit hit;
-
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(rBody.transform.position, rBody.transform.TransformDirection(Vector3.right), rayDistance))
-        {
-            Debug.DrawRay(rBody.transform.position, rBody.transform.TransformDirection(Vector3.right) * rayDistance, Color.yellow);
-           // Debug.Log("Did Hit");
-            availableJump = true;
-        }
-        else
-        {
-            Debug.DrawRay(rBody.transform.position, rBody.transform.TransformDirection(Vector3.right) * rayDistance, Color.white);
-           // Debug.Log("Did not Hit");
-        }
-
-        //movementVector.y = Input.GetAxis("Vertical") * jumpStrength_;
-
-
-
 
         rBody.AddForce(movementVector);
     }
@@ -53,7 +31,7 @@ public class BiMovementForcedB : CMotor
 
     protected override void ConfigurePhysics()
     {
-        
+
         rBody = GetComponent<Rigidbody>();
         transform.rotation = Quaternion.Euler(new Vector3(180f, -90f, 90f));
         rBody.constraints =
@@ -62,10 +40,11 @@ public class BiMovementForcedB : CMotor
             RigidbodyConstraints.FreezeRotationX |
             RigidbodyConstraints.FreezeRotationY |
             RigidbodyConstraints.FreezePositionZ;
-        
+
+
         rBody.useGravity = true;
-        
+
         rBody.interpolation = RigidbodyInterpolation.Interpolate;
-        
+
     }
 }
