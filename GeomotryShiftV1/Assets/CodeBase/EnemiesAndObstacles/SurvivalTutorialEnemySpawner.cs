5a1ff3e7cd29adb50bdbb9e89f79b2819c2a238d
azou1 <azou1@myseneca.com>
2019-03-14 15:19:25 -0400

    small bug fix (stuck phase)

--- GeomotryShiftV1/Assets/CodeBase/EnemiesAndObstacles/SurvivalTutorialEnemySpawner.cs
+++ GeomotryShiftV1/Assets/CodeBase/EnemiesAndObstacles/SurvivalTutorialEnemySpawner.cs
@@ -9,6 +9,7 @@ public class SurvivalTutorialEnemySpawner : SurvivalLevel1EnemySpawner
     // Start is called before the first frame update
     void Start()
     {
+        phase = 1;
         LoadEnemiesFromConglomerate();
         SetupEnemyDefaultVariables();
 
