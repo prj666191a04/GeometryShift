2839b271e784a274532c236990626d29fc77ff42
atilla17 <42915217+atilla17@users.noreply.github.com>
2019-03-18 03:42:11 -0400

    Adjustments to several levels

--- GeomotryShiftV1/Assets/CodeBase/Levels/LevelEKill.cs
+++ GeomotryShiftV1/Assets/CodeBase/Levels/LevelEKill.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//Author Harly Sims, Edited by Atilla Puskas
+//controlling script for LevelE
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +8,33 @@ public class LevelEKill : LevelBase
 {
     public Transform cameraPoint;
     public GameObject spawn;
+    public LevelOverlayUI levelUi;
+    
+
+
+    private void ResetLevel(int method = 0)
+    {
+        levelUi.ShowRetryScreen();
+    }
 
     void OnEnable()
     {
-        CStatus.OnPlayerDeath += foo;
-
+        CStatus.OnPlayerDeath += ResetLevel;
+        LevelOverlayUI.OnResultScreenFinished += base.AcknowledgeLevelCompletion;
+        LevelOverlayUI.OnRetryRequested += foo;
+        LevelOverlayUI.OnLevelQuit += TerminateLevelAttempt;
     }
 
     void OnDisable()
     {
-        CStatus.OnPlayerDeath -= foo;
+        CStatus.OnPlayerDeath -= ResetLevel;
+        LevelOverlayUI.OnResultScreenFinished -= base.AcknowledgeLevelCompletion;
+        LevelOverlayUI.OnRetryRequested -= foo;
+        LevelOverlayUI.OnLevelQuit -= TerminateLevelAttempt;
     }
 
-    private void foo(int i =0) {
+    // Note: for Harley sims, I would have expected for you to at rename sample functions I give you to have meaningful names once in production.
+    private void foo() {
         //Debug.Log("Enter Foo");
         StartCoroutine(playerRespawn());
     }
@@ -30,5 +46,10 @@ public class LevelEKill : LevelBase
         yield return new WaitForSeconds(3);
         GeometryShift.playerStatus.gameObject.GetComponent<CController>().Respawn(spawn.transform.position, true);
         yield break;
+    }
+    public override void AcknowledgeLevelCompletion(int code = 0)
+    {
+        levelUi.ShowRsltScreen("", code);
+        LevelRessultScreenSendMessage();
     }
 }
