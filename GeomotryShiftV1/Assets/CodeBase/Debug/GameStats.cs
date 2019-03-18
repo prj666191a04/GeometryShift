8feca6c2147bb50f6a1b916717209da7d9df8a9c
atilla17 <42915217+atilla17@users.noreply.github.com>
2019-03-18 18:19:59 -0400

    Fixed root menue canvas and experimented with some project settings.

---
+++ GeomotryShiftV1/Assets/CodeBase/Debug/GameStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStats : MonoBehaviour
+{
+    public Text fpsCounter;
+    float frameRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+      // Debug.Log(QualitySettings.vSyncCount);
+      // QualitySettings.vSyncCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //  frameRate = 1.0f / Time.deltaTime;
+       //  fpsCounter.text = frameRate.ToString();
+    }
+}
