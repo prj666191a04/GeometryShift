87c754f2b13ed4b4bede2c521ef28e3f40f92872
boohar02 <46663574+boohar02@users.noreply.github.com>
2019-04-06 14:25:30 -0400

    Finsiehd TOS screen

---
+++ GeomotryShiftV1/Assets/CodeBase/TOS.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TOS : MonoBehaviour
+{
+
+    public Button acceptBtn, declineBtn;
+
+    void Start()
+    {
+        acceptBtn.onClick.AddListener(AcceptTOS);
+        declineBtn.onClick.AddListener(DeclineTOS);
+    }
+
+    void AcceptTOS()
+    {
+        Debug.Log("Accept TOS");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameTestA");
+    }
+
+    void DeclineTOS()
+    {
+        Debug.Log("Declined TOS");
+        Application.Quit();
+    }
+
+}
